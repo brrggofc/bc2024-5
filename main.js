35cadba1f56f30233d06a300a3043215377f789d
Михайло bc2024-5 <krasavaaabuster@gmail.com>
2024-11-27 12:53:41 +0200

    part 2

--- main.js
+++ main.js
@@ -16,3 +16,28 @@ const app = express();
 app.listen(options.port, options.host, () => {
   console.log(`Server running at http://${options.host}:${options.port}`);
 });
+
+app.get('/notes/:name', (req, res) => {
+    // Логіка для отримання нотатки
+  });
+
+  app.put('/notes/:name', (req, res) => {
+    // Логіка для оновлення нотатки
+  });
+
+  app.delete('/notes/:name', (req, res) => {
+    // Логіка для видалення нотатки
+  });
+
+  app.get('/notes', (req, res) => {
+    // Логіка для отримання списку нотаток
+  });
+
+  app.post('/write', express.urlencoded({ extended: true }), (req, res) => {
+    // Логіка для створення нової нотатки
+  });
+
+  const path = require('path');
+app.get('/UploadForm.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'UploadForm.html'));
+});
